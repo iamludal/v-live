4fc7e4ba45280e067119e6298eafb81416d3f7a5
Ludal <ludovic.chombeau@gmail.com>
2020-03-03 22:41:28 +0100

    Update Stations (complete) + add station as parameter of create_station()

--- lib/Station.class.php
+++ lib/Station.class.php
@@ -9,10 +9,21 @@ class Station
     {
         $fields = $station->fields;
         $this->name = $fields->nom;
-        $this->city = $fields->commune;
-        $this->state = $fields->etat;
-        $this->connectionState = $fields->etatconnexion;
+        $this->city = ucwords(strtolower($fields->commune));
         $this->bikes = $fields->nbvelosdispo;
         $this->slots = $fields->nbplacesdispo;
+        $this->address = ucwords(strtolower($fields->adresse));
+        $this->state = ucwords(strtolower($fields->etat));
+        $this->geo = $fields->geo;
+
+        $connectionStates = array(
+            "CONNECTED" => "En ligne",
+            "DISCONNECTED" => "Hors ligne"
+        );
+
+        $tpeValues = array("AVEC TPE" => "Oui", "SANS TPE" => "Non");
+
+        $this->connectionState = $connectionStates[$fields->etatconnexion];
+        $this->tpe = $tpeValues[$fields->type];
     }
 }
--- lib/functions.php
+++ lib/functions.php
@@ -3,30 +3,26 @@
 /**
  * Return the HTML code corresponding to a station
  * 
- * @param string $name        the name of the station
- * @param string $city        the city of the station
- * @param int $availableBikes the number of available bikes in the station
- * @param int $freeSlots      the number of free slots in the station
- * @param bool $highlight     true if the station should be highlighted
+ * @param Station $station the station object from which to create the html
  * @return string the HTML code of the station
  */
-function create_station($name, $city, $availableBikes, $freeSlots, $highlight = false)
+function create_station($station, $highlight = false)
 {
-    // Convert city to Title Case
-    $city = ucwords(strtolower($city));
     $class = $highlight ? "highlight" : "";
+    $geo = json_encode($station->geo);
 
-    $html = "<div class=\"station card $class\">";
+    $html = "<div class=\"station card $class\" data-geo=\"$geo\" ";
+    $html .=                                   "data-name=\"$station->name\">";
     $html .=   '<span class="station-title">';
     $html .=      '<i class="fas fa-map-marker-alt"></i>';
-    $html .=        " $name ･ $city ";
+    $html .=        " $station->name ･ $station->city ";
     $html .=    '</span>';
     $html .=   '<span>';
     $html .=      '<i class="fas fa-check-circle"></i>';
     $html .=      ' Libres: ';
-    $html .=       "<span class=\"station-bikes\">$availableBikes</span> Vélos";
+    $html .=       "<span class=\"station-bikes\">$station->bikes</span> Vélos";
     $html .=       ' | ';
-    $html .=       "<span class=\"station-slots\">$freeSlots</span> Emplacements";
+    $html .=       "<span class=\"station-slots\">$station->slots</span> Emplacements";
     $html .=    '</span>';
     $html .= '</div>';
 
