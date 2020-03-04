1e8f1659046bac55b02453198e6d90a93a9fabfd
Ludal <ludovic.chombeau@gmail.com>
2020-03-04 15:19:07 +0100

    Change FA icons + create_info + add data to station

--- lib/functions.php
+++ lib/functions.php
@@ -12,13 +12,20 @@ function create_station($station, $highlight = false)
     $geo = json_encode($station->geo);
 
     $html = "<div class=\"station card $class\" data-geo=\"$geo\" ";
+    $html .=                                   "data-address=\"$station->address\" ";
+    $html .=                                   "data-city=\"$station->city\" ";
+    $html .=                                   "data-bikes=\"$station->bikes\" ";
+    $html .=                                   "data-slots=\"$station->slots\" ";
+    $html .=                                   "data-state=\"$station->state\" ";
+    $html .=                                   "data-connectionstate=\"$station->connectionState\" ";
+    $html .=                                   "data-tpe=\"$station->tpe\" ";
     $html .=                                   "data-name=\"$station->name\">";
     $html .=   '<span class="station-title">';
-    $html .=      '<i class="fas fa-map-marker-alt"></i>';
+    $html .=      '<i class="fa fa-map-marker"></i>';
     $html .=        " $station->name ･ $station->city ";
     $html .=    '</span>';
     $html .=   '<span>';
-    $html .=      '<i class="fas fa-check-circle"></i>';
+    $html .=      '<i class="fa fa-check-circle"></i>';
     $html .=      ' Libres: ';
     $html .=       "<span class=\"station-bikes\">$station->bikes</span> Vélos";
     $html .=       ' | ';
@@ -28,3 +35,34 @@ function create_station($station, $highlight = false)
 
     return $html;
 }
+
+/**
+ * Create the HTML code corresponding to an information
+ * 
+ * @param string $icon the icon to use (font awesome)
+ * @param string $property the property of the information
+ * @param string $title the title of the information
+ * @return string the HTML code corresponding to the station
+ */
+function create_info($icon, $property, $title)
+{
+    $html = "<div class=\"info card\" data-property=\"$property\">";
+    $html .=   '<span class="info-title">';
+    $html .=      "<i class=\"fa $icon\"></i> $title";
+    $html .=   '</span>';
+    $html .=   "<span class=\"info-value\"></span>";
+    $html .= '</div>';
+    return $html;
+}
+
+/* 
+<div class="info card">
+    <span class="info-title">
+        <i class="fa fa-cog"></i>
+        État
+    </span>
+    <span class="info-value">En service</span>
+</div>
+
+
+*/
--- views/home.php
+++ views/home.php
@@ -3,7 +3,8 @@
 require_once(dirname(__DIR__) . '/lib/functions.php');
 require_once(dirname(__DIR__) . '/lib/Station.class.php');
 
-$stations = json_decode(file_get_contents("http://vlille.fil.univ-lille1.fr"));
+$stations = json_decode(file_get_contents(__DIR__ . '/data.json'));
+// $stations = json_decode(file_get_contents("http://vlille.fil.univ-lille1.fr"));
 
 ?>
 
@@ -20,18 +21,18 @@ $stations = json_decode(file_get_contents("http://vlille.fil.univ-lille1.fr"));
 
     <!-- Leaflet -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
+    <link rel="stylesheet" href="styles/font-awesome/css/font-awesome.min.css">
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
 </head>
 
 <body>
     <header>
         <h1 class="main-title">
-            <i class="fas fa-biking"></i> V'Live
+            <i class="fa fa-bicycle"></i> V'Live
         </h1>
-
         <button class="search subtitle" id="show-modal">
             <span>Recherche</span>
-            <i class="fas fa-search"></i>
+            <i class="fa fa-search"></i>
         </button>
         </form>
     </header>
@@ -39,73 +40,36 @@ $stations = json_decode(file_get_contents("http://vlille.fil.univ-lille1.fr"));
     <main>
         <div class="stations">
             <h2 class="subtitle">
-                Stations <i class="fas fa-directions"></i>
+                Stations <i class="fa fa-map-signs"></i>
             </h2>
 
             <div class="stations-container">
-
-                <?php
-
-                foreach ($stations as $station) {
-                    echo create_station(new Station($station));
-                }
-
-                ?>
+                <?php foreach ($stations as $station) : ?>
+                    <?= create_station(new Station($station)) ?>
+                <?php endforeach ?>
             </div>
         </div>
         <div class="map" id="leaflet-map">
         </div>
         <div class="infos">
             <h2 class="subtitle">
-                <i class="fas fa-info-circle"></i> Infos
+                <i class="fa fa-info-circle"></i> Infos
             </h2>
 
             <div class="infos-container">
                 <div class="infos-main-title card highlight">
-                    <span class="name">DE GAULLE</span>
-                    <span class="city"><i class="fas fa-map-marker-alt"></i> LA MADELEINE </span>
-                </div>
-                <!--// create_infos() -->
-                <div class="info card">
-                    <span class="info-title">
-                        <i class="fas fa-thumbtack"></i>
-                        Adresse
+                    <span class="name"></span>
+                    <span>
+                        <i class="fa fa-map-marker"></i> <span class="city"></span>
                     </span>
-                    <span class="info-value">318, RUE ROGER SALENGRO</span>
                 </div>
-                <div class="info card">
-                    <span class="info-title">
-                        <i class="fas fa-biking"></i> Vélos disponibles
-                    </span>
-                    <span class="info-value">2</span>
-                </div>
-                <div class="info card">
-                    <span class="info-title">
-                        <i class="fas fa-parking"></i>
-                        Emplacements disponibles
-                    </span>
-                    <span class="info-value">8</span>
-                </div>
-                <div class="info card">
-                    <span class="info-title">
-                        <i class="fas fa-credit-card"></i>
-                        TPE
-                    </span>
-                    <span class="info-value">Oui</span>
-                </div>
-                <div class="info card">
-                    <span class="info-title">
-                        <i class="fas fa-cog"></i>
-                        État
-                    </span>
-                    <span class="info-value">En service</span>
-                </div>
-                <div class="info card">
-                    <span class="info-title">
-                        <i class="fas fa-wifi"></i> État Connexion
-                    </span>
-                    <span class="info-value">En Ligne</span>
-                </div>
+
+                <?= create_info("fa-thumb-tack", "address", "Adresse") ?>
+                <?= create_info("fa-bicycle", "bikes", "Vélos disponibles") ?>
+                <?= create_info("fa-check", "slots", "Emplacements disponibles") ?>
+                <?= create_info("fa-credit-card", "tpe", "Paiement par carte") ?>
+                <?= create_info("fa-cog", "state", "État") ?>
+                <?= create_info("fa-wifi", "connectionstate", "État connexion") ?>
             </div>
         </div>
     </main>
@@ -116,8 +80,8 @@ $stations = json_decode(file_get_contents("http://vlille.fil.univ-lille1.fr"));
         <span>Copyright © 2020 Ludovic Chombeau - Romain Follet</span>
         <a class="link" href="">Crédits</a>
     </footer>
-
-    <script src="//kit.fontawesome.com/c7aac9c8ff.js" crossorigin="anonymous"></script>
+    <!-- 
+    <script src="//kit.fontawesome.com/c7aac9c8ff.js" crossorigin="anonymous"></script> -->
     <script src="js/map.js"></script>
 
 </body>
