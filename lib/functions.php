e9f8081ef1135ac67fa066d6a86a261ff55e5e53
Ludal <ludovic.chombeau@gmail.com>
2020-03-07 23:34:44 +0100

    Bring back heredoc

--- lib/functions.php
+++ lib/functions.php
@@ -6,25 +6,21 @@
  * @param Station $station the station object from which to create the html
  * @return string the HTML code of the station
  */
-function create_station($station, $highlight = false)
+function create_station($station)
 {
-    $class = $highlight ? "highlight" : "";
     $geo = json_encode($station->geo);
 
-    $html = "<li class=\"station card $class\" data-geo=\"$geo\" ";
-    $html .=                                   "data-address=\"$station->address\" ";
-    $html .=                                   "data-city=\"$station->city\" ";
-    $html .=                                   "data-bikes=\"$station->bikes\" ";
-    $html .=                                   "data-slots=\"$station->slots\" ";
-    $html .=                                   "data-state=\"$station->state\" ";
-    $html .=                                   "data-name=\"$station->name\">";
-    $html .=   '<span class="station-title">';
-    $html .=      '<i class="fa fa-map-marker"></i>';
-    $html .=        " <strong>{$station->bikes}V | {$station->slots}P</strong> $station->name ･ $station->city ";
-    $html .=    '</span>';
-    $html .= '</li>';
-
-    return $html;
+    return <<<HTML
+    <li class="station card" data-geo="$geo" data-address="{$station->address}"
+                        data-bikes="{$station->bikes}" data-slots="{$station->slots}"
+                        data-state="{$station->state}" data-name="{$station->name}">
+        <span class="station-title">
+            <i class="fa fa-map-farker"></i>
+            <strong>{$station->bikes}V | {$station->slots}P</strong>
+            {$station->name} ･ {$station->city}
+        </span>
+    </li>
+HTML;
 }
 
 /**
@@ -37,13 +33,14 @@ function create_station($station, $highlight = false)
  */
 function create_info($icon, $property, $title)
 {
-    $html = "<li class=\"info card\" data-property=\"$property\">";
-    $html .=   '<span class="info-title">';
-    $html .=      "<i class=\"fa $icon\"></i> $title";
-    $html .=   '</span>';
-    $html .=   "<span class=\"info-value\"></span>";
-    $html .= '</li>';
-    return $html;
+    return <<<HTML
+    <li class="info card" data-property="$property">
+        <span class="info-title">
+            <i class="fa $icon"></i> $title
+        </span>
+        <span class="info-value"><span>
+    </li>
+HTML;
 }
 
 /**
