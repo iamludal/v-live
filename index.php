afe9b530013b08dd9bacee219df31c7259696c44
Ludal <ludovic.chombeau@gmail.com>
2020-03-07 22:15:40 +0100

    Station style: code style improvement

--- index.php
+++ index.php
@@ -2,16 +2,14 @@
 
 require_once(__DIR__ . '/lib/functions.php');
 require_once(__DIR__ . '/lib/Station.class.php');
+require_once(__DIR__ . '/lib/verifyParams.php');
 
-$stations = json_decode(file_get_contents("data.json"));
-// $stations = json_decode(file_get_contents("http://vlille.fil.univ-lille1.fr"));
+$url = "http://vlille.fil.univ-lille1.fr";
+$params = "commune=$city&nom=$name&nbvelosdispo=$bikes&nbplacesdispo=$slots&etat=$state";
+$url .= "?$params";
+
+$stations = json_decode(file_get_contents($url));
 $stations = array_map("convert_to_station_object", $stations);
-
-$sort = filter_input(INPUT_GET, 'sort', FILTER_SANITIZE_STRING, [
-    "options" => [
-        "default" => "name"
-    ]
-]);
 
 if ($sort === 'name') $cmp_func = "compare_stations_by_name";
 elseif ($sort === 'city') $cmp_func = "compare_stations_by_city";
